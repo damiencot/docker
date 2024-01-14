506a5b5574ade8424363e8a910d70b0a3b2ef7bd
damiencot <damien.cot@gmail.com>
2024-01-14 09:52:41 +0100

    initial

---
+++ src/test/java/com/example/applicationun/ApplicationUnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationUnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
